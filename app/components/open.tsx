a760324b936e4e2f5e5fc5b29469acbbe9f504ae
techwizard31 <istaprasad.patra@gmail.com>
2025-02-28 22:32:54 +0530

    better

--- app/components/open.tsx
+++ app/components/open.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 function Open() {
     const router = useRouter();
   return (
-    <div className='w-full h-screen text-white bg-black'>
+    <div className='w-full h-screen text-white bg-gradient-to-r from-[#2E073F] via-[#000B58] to-[#2E073F]'>
         <h1 className='font-oxanium text-7xl text-center pt-10'>Jobify</h1>
         <div className='flex w-full h-[calc(100vh-5rem)] flex-row items-center justify-center gap-20'>
             <div className='w-1/5 h-44 font-oxanium text-4xl border-2 border-white text-center rounded-3xl cursor-pointer hover:scale-105 transition-all duration-150 flex justify-center items-center'onClick={()=>router.push("/company/register")}>HireZone</div>
